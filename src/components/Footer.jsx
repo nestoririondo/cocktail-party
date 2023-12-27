d247721c5359991aa9a31e62f335531efebdf483
Nestor Iriondo <nestoririondo@gmail.com>
2023-12-27 18:53:30 +0100

    footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,21 @@
+import githubMark from '../github-mark.svg';
 
 const Footer = () => {
     return ( 
         <div className="footer">
             <div className="about">
                 <p>
-                    <strong>Drink Finder</strong> is a web application built with React that allows users to search for cocktails by name. The application uses the <a href="https://www.thecocktaildb.com/api.php" target="_blank" rel="noreferrer">CocktailDB API</a> to fetch cocktail data.
+                    Cocktail party is a web application built with React that allows users to search for cocktails by name. The application uses the <a href="https://www.thecocktaildb.com/api.php" target="_blank" rel="noreferrer">CocktailDB API</a> to fetch cocktail data.
                 </p>
 
             </div>
             <div className="contact">
                 <p>
-                    <strong>Developer:</strong> <a href="
+                    Developed by <a href="
                     http://www.nestoririondo.com" target="_blank" rel="noreferrer">Néstor Iriondo</a>
                 </p>
-                <p>
-                    <strong>GitHub:</strong> <a href="
-                    https://github.com/nestoririondo" target="_blank" rel="noreferrer">nestoririondo</a>
-                </p>
+                <a href="https://github.com/nestoririondo"><img src={githubMark} /></a> 
+
             </div>
 
 
